cd333a6678d6475acfce61a95fd5d3c3d4924a94
gacmalony <gocmenmertkaan@gmail.com>
2024-01-20 19:32:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/imagetotext/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.imagetotext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.idBtnCapture);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ScanerActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
